caa575416bd241938011f18ea5a3d1affd74131a
bagool185 <ioan.adrian98@gmail.com>
2017-11-30 00:28:00 +0000

    adapted plot to not contain occurrence and solved minor bug

--- statistics.py
+++ statistics.py
@@ -13,28 +13,30 @@ class Statistics(metaclass=Singleton):
         self.k_occurrence = np.zeros(16)  # List initialised with 0 that will count the number of tests of given k.
         self.k_accuracy = np.zeros(16)  # List initialised with 0 that will store the accuracy of given k.
 
-    def generate_plots(self, occurrence=None, accuracy=None):
+    def generate_plots(self, accuracy=None):
         """ Generate and display the plots for the current tests (if the method is called without parameters) or for
             data loaded from a given file (if the method is called with parameters)
         """
 
-        if not isinstance(occurrence, list):
+        temp_accuracy = list()
+
+        if isinstance(accuracy, list):
+            temp_accuracy.extend(accuracy)
+        else:
             occurrence = self.k_occurrence[0:]
             accuracy = self.k_accuracy[0:]
 
-        temp_accuracy = []
+            temp_accuracy.extend(accuracy)  # Store the accuracy in a temporal vector so it won't be divided each time.
 
-        temp_accuracy.extend(accuracy)  # Store the accuracy in a temporal vector so it won't be divided each time.
-
-        for k in range(16):  # Calculate the accuracy for each k.
-            if occurrence[k] > 0:
-                temp_accuracy[k] /= occurrence[k]
+            for k in range(16):  # Calculate the accuracy for each k.
+                if occurrence[k] > 0:
+                    temp_accuracy[k] /= occurrence[k]
 
         plt.gca().clear()  # Clear the plot area.
         plt.scatter(range(16), temp_accuracy, label='current accuracy', color='g')
 
         plt.xlabel('k')
-        plt.ylabel('accuracy and occurance')
+        plt.ylabel('accuracy')
         plt.title("Accuracy rate plot")
 
         plt.axis([1, 16, 0.0, 1.0])
@@ -51,8 +53,7 @@ class Statistics(metaclass=Singleton):
         try:
             file_path = QFileDialog.getOpenFileName(None, 'OpenFile', '.', 'CSV files (*.csv)')
 
-            k_occurrence = [0 for k in range(16)]
-            k_accuracy = [0 for k in range(16)]
+            k_accuracy = np.zeros(16)
 
             with open(file_path[0], "r") as csv_file:
                 try:
@@ -60,24 +61,20 @@ class Statistics(metaclass=Singleton):
 
                     for row in lines:  # Read the file line by line.
                         # The lines are structured as follows:
-                        # k, number of tests ran of k, accuracy of tests ran with k
+                        # k, accuracy of tests ran with k
                         k = int(row[0])
-                        occurrence = int(row[1])
-                        accuracy = float(row[2])
+                        accuracy = float(row[1])
 
-                        k_occurrence[k] = occurrence
-                        k_accuracy[k] = accuracy
+                        k_accuracy[k-1] = accuracy  # k - 1 because normally the list is shifted one place to the left
 
-                        self.generate_plots(k_occurrence, k_accuracy)
+                    self.generate_plots(k_accuracy)
 
                 except IOError as e:
                     QMessageBox.warning(None, "Loading data error",
                                         f"There has been an error reading the data from the given file.\n{e}")
 
-        except:
-            # Usually bad practice, but an error will be raised when the user quits the open file dialog, so it has to
-            # be passed, as it doesn't affect the functionality of the program, nor should it be considered an error
-            # in this context
+        except IndexError:
+            QMessageBox.warning(None, "Wrong csv file format", "Sorry, the selected csv file format isn't valid.")
             pass
 
     def save_in_file(self):
@@ -93,7 +90,7 @@ class Statistics(metaclass=Singleton):
             with open(file_path[0], "w") as csv_file:
                 try:
                     for k in range(1, 16, 1):
-                        csv_file.write(f"{k},{int(self.k_occurrence[k])},{self.k_accuracy[k]}\n")
+                        csv_file.write(f"{k},{self.k_accuracy[k]}\n")
 
                 except IOError as e:
                     QMessageBox.warning(None, "Saving file error",
